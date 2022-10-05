b53a2eba7977015d65a5d797f17ac9087617eed4
RollaT <rturkoane01@gmail.com>
2022-10-05 12:30:53 -0400

    Created Three Classes and wrote code

--- src/RollaT/Class1.java
+++ src/RollaT/Class1.java
@@ -1,5 +1,19 @@
 package RollaT;
 
+import java.util.Scanner;
+
 public class Class1 {
-    //1. Assignment 1
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number");
+        int x = input.nextInt();
+        System.out.println("Enter second number");
+        int y = input.nextInt();
+        System.out.println("After swapping" );
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("value of x: " + x);
+        System.out.println("value of y: " + y);
+    }
 }
--- src/RollaT/Class2.java
+++ src/RollaT/Class2.java
@@ -1,6 +1,44 @@
 package RollaT;
 
 public class Class2 {
-    //Task 2;
+    public static void main(String[] args) {
+        solution(45);
+    }
+    public static void solution(int n) {
+        String divisibleBy15 = "";
+        String divisibleBy5 = "";
+        String divisibleBy3 = "";
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int each : arr) {
+
+            if (each % 15 == 0 && each % 3 == 0) {
+                divisibleBy15 += each + " ";
+            }
+
+            if (each % 5 == 0 && each % 15 != 0) {
+                divisibleBy5 += each + " ";
+            }
+
+            if (each % 3 == 0 && each % 15 != 0) {
+                divisibleBy3 += each + " ";
+            }
+
+        }
+
+        System.out.println("Divisible By 15: " + divisibleBy15);
+        System.out.println("Divisible By 5: " + divisibleBy5);
+        System.out.println("Divisible By 3: " + divisibleBy3);
+    }
+
 
 }
+
+
+
+
